6f5ccbe19f43a8ca627fd013278686a3151cdb75
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-05-17 21:48:56 +0900

    장바구니 제품 수량 변경 및 제거 로직 구성 / 2024-05-17

--- lib/item_basket_page.dart
+++ lib/item_basket_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shoppingmall/item_checkout_page.dart';
@@ -12,34 +14,57 @@ class ItemBasketPage extends StatefulWidget {
 }
 
 class _ItemBasketPageState extends State<ItemBasketPage> {
-  List<Product> basketList = [
+  List<Product> productList = [
     Product(
-      productNo: 1,
-      productName: "노트북(Laptop)",
-      productImageUrl: "https://picsum.photos/id/1/300/300",
-      price: 600000,
-    ),
+        productNo: 1,
+        productName: "노트북(Laptop)",
+        productImageUrl: "https://picsum.photos/id/1/300/300",
+        price: 600000),
     Product(
-      productNo: 4,
-      productName: "키보드(Keyboard)",
-      productImageUrl: "https://picsum.photos/id/60/300/300",
-      price: 50000,
-    ),
-  ];
-
-  List<Map<int, int>> quantityList = [
-    {1: 2},
-    {4: 3},
+        productNo: 2,
+        productName: "스마트폰(Phone)",
+        productImageUrl: "https://picsum.photos/id/20/300/300",
+        price: 500000),
+    Product(
+        productNo: 3,
+        productName: "머그컵(Cup)",
+        productImageUrl: "https://picsum.photos/id/30/300/300",
+        price: 15000),
+    Product(
+        productNo: 4,
+        productName: "키보드(Keyboard)",
+        productImageUrl: "https://picsum.photos/id/60/300/300",
+        price: 50000),
+    Product(
+        productNo: 5,
+        productName: "포도(Grape)",
+        productImageUrl: "https://picsum.photos/id/75/200/300",
+        price: 75000),
+    Product(
+        productNo: 6,
+        productName: "책(book)",
+        productImageUrl: "https://picsum.photos/id/24/200/300",
+        price: 24000),
   ];
 
   double totalPrice = 0;
+  Map<String, dynamic> cartMap = {};
 
   @override
   void initState() {
     super.initState();
-    for (int i = 0; i < basketList.length; i++) {
-      totalPrice += basketList[i].price! * quantityList[i][basketList[i].productNo]!;
+
+    // 저장한 장바구니 리스트 가져오기
+    cartMap = json.decode(sharedPreferences.getString("cartMap") ?? "{}") ?? {};
+  }
+
+  double calculateTotalPrice() {
+    totalPrice = 0;
+    // 총액 계산
+    for (int i = 0; i < cartMap.length; i++) {
+      totalPrice += productList.firstWhere((element) => element.productNo == int.parse(cartMap.keys.elementAt(i))).price! * cartMap[cartMap.keys.elementAt(i)];
     }
+    return totalPrice;
   }
 
   @override
@@ -50,14 +75,19 @@ class _ItemBasketPageState extends State<ItemBasketPage> {
         centerTitle: true,
       ),
       body: ListView.builder(
-        itemCount: basketList.length,
+        itemCount: cartMap.length,
         itemBuilder: (context, index) {
+          int productNo = int.parse(cartMap.keys.elementAt(index));
+          Product currentProduct = productList.firstWhere((element) {
+            return element.productNo == productNo;
+          });
+
           return basketContainer(
-              productNo: basketList[index].productNo ?? 0,
-              productName: basketList[index].productName ?? "",
-              productImageUrl: basketList[index].productImageUrl ?? "",
-              price: basketList[index].price ?? 0,
-              quantity: quantityList[index][basketList[index].productNo] ?? 0);
+              productNo: productNo,
+              productName: currentProduct.productName ?? "",
+              productImageUrl: currentProduct.productImageUrl ?? "",
+              price: currentProduct.price ?? 0,
+              quantity: cartMap[productNo.toString()]);
         },
       ),
       bottomNavigationBar: Padding(
@@ -71,7 +101,7 @@ class _ItemBasketPageState extends State<ItemBasketPage> {
                 },
             ));
           },
-          child: Text("총 ${numberFormat.format(totalPrice)}원 결제하기"),
+          child: Text("총 ${numberFormat.format(calculateTotalPrice())}원 결제하기"),
         ),
       ),
     );
@@ -92,6 +122,7 @@ class _ItemBasketPageState extends State<ItemBasketPage> {
           CachedNetworkImage(
               imageUrl: productImageUrl,
             width: MediaQuery.of(context).size.width * 0.3,
+            height: 130,
             fit: BoxFit.cover,
             placeholder: (context, url) {
                 return const Center(
@@ -124,20 +155,49 @@ class _ItemBasketPageState extends State<ItemBasketPage> {
                   children: [
                     const Text("수랑:"),
                     IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          // 수량 줄이기 (1 초과시에만 감소시킬 수 있음)
+                          if (cartMap[productNo.toString()] > 1) {
+                            setState(() {
+                              // 수량 1 차감
+                              cartMap[productNo.toString()]--;
+
+                              // 디스크에 반영
+                              sharedPreferences.setString("cartMap", json.encode(cartMap));
+                            });
+                          }
+                        },
                         icon: const Icon(
                           Icons.remove,
                         ),
                     ),
                     Text("$quantity"),
                     IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            // 수량 늘리기
+                            cartMap[productNo.toString()]++;
+
+                            // 디스크에 반영
+                            sharedPreferences.setString(
+                                "cartMap", json.encode(cartMap));
+                          });
+                        },
                         icon: const Icon(
                           Icons.add,
                         ),
                     ),
                     IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            // 장바구니에서 해당 제품 제거
+                            cartMap.remove(productNo.toString());
+
+                            // 디스크에 반영
+                            sharedPreferences.setString(
+                                "cartMap", json.encode(cartMap));
+                          });
+                        },
                         icon: const Icon(
                           Icons.delete,
                         ),
